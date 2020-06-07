6cf4122755e846f80fdbe1d7d73f8dfbf0f478a7
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2020-06-07 23:25:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,36 +7,53 @@ Created on Sun Jun  7 17:20:30 2020
 
 import filters
 import img_io
+import canny_detector
+
+img_name = "img.jpg"
+
+img = filters.apply_filter(img_name, filter = 'average', display = True, read_method = 1, flip_scheme = True)
+img_io.save_image(img, 'rgb', edit_name = 'flipped_blur_avg')
 
 
-img = "IMG_0624.JPG"
-"""
-the apply_filter function has the paramters:
-1) img (string): the image name
-2) filter (string) : the filter to be applied ('median', 'gaussian', 'average'). default is 'average'
-3) kernel_size (int) : the size of the kernel which would be used for convolution. default size is 5
-4) display (bool): if set to true, displays the image. default false
-"""
-
-"""
-the save_image function has the following parameters:
-1) img (numpy.ndarray): the matrix that would be converted to an image
-2) img_type (string): the image can be saved in two formats ('RGB', 'grayscale'). currently only 'RGB' supported.
-3) height (int): the height of the saved image
-4) width (int): the width of the saved image
-5) edit_name (string): the filename of the saved image default 'image'
-6) extension (string): the extension of the image file. default '.jpg'
-7) show (bool): whether to display the image. default false
-"""
+img = filters.apply_filter(img_name, filter = 'average', display = True, read_method = 1, flip_scheme = False)
+img_io.save_image(img, 'rgb', edit_name = 'blur_avg')
 
 
-img_med = filters.apply_filter(img, filter = 'median', kernel_size = 5, display = True)
-img_io.save_image(img_med, img_type = 'RGB', height = 3000 , width = 4000, edit_name = 'median_blur')
+
+img = filters.apply_filter(img_name, filter = 'gaussian', display = True, read_method = 1, flip_scheme = True)
+img_io.save_image(img, 'rgb', edit_name = 'flipped_blur_gaussian')
 
 
-img_gauss = filters.apply_filter(img, filter = 'gaussian', kernel_size = 5, display = True)
-img_io.save_image(img_gauss, img_type = 'RGB', height = 3000 , width = 4000, edit_name = 'gaussian_blur')
+img = filters.apply_filter(img_name, filter = 'gaussian', display = True, read_method = 1, flip_scheme = False)
+img_io.save_image(img, 'rgb', edit_name = 'blur_gaussian')
 
 
-img_average = filters.apply_filter(img, filter = 'average', kernel_size = 5, display = True)
-img_io.save_image(img_average, img_type = 'RGB',height = 3000 , width = 4000, edit_name = 'average_blur')
+
+img = filters.apply_filter(img_name, filter = 'median', display = True, read_method = 1, flip_scheme = True)
+img_io.save_image(img, 'rgb', edit_name = 'flipped_blur_median')
+
+
+img = filters.apply_filter(img_name, filter = 'median', display = True, read_method = 1, flip_scheme = False)
+img_io.save_image(img, 'rgb', edit_name = 'blur_median')
+
+
+
+
+img = filters.apply_filter(img_name, filter = 'gaussian', display = True, read_method = 1, flip_scheme = True)
+img = canny_detector.auto_canny(img)
+img_io.save_image(img, 'grayscale', edit_name = 'edge_flipped_blur_gaussian')
+
+
+img = filters.apply_filter(img_name, filter = 'gaussian', display = True, read_method = 1, flip_scheme = False)
+img = canny_detector.auto_canny(img)
+img_io.save_image(img, 'grayscale', edit_name = 'edge_blur_gaussian')
+
+
+img = filters.apply_filter(img_name, filter = 'gaussian', display = True, read_method = 1, flip_scheme = True)
+img = canny_detector.auto_canny(img, method = 'mode')
+img_io.save_image(img, 'grayscale', edit_name = 'edge_flipped_blur_gaussian_method_mode')
+
+
+img = filters.apply_filter(img_name, filter = 'gaussian', display = True, read_method = 1, flip_scheme = False)
+img = canny_detector.auto_canny(img, method = 'mode')
+img_io.save_image(img, 'grayscale', edit_name = 'edge_blur_gaussian_method_mode')
