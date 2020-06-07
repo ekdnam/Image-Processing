0b29f62c44e3bfd2d498d0a8492662e8a9845610
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2020-06-07 19:08:21 +0530

    Add files via upload

---
+++ filters/filters.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 16:56:53 2020
+
+@author: vgadi
+"""
+from PIL import Image
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
+
+def apply_filter(img_name,
+                 filter = 'average',
+                 kernel_size = 5, 
+                 display = False,
+                 ):
+    # get image
+    try:    
+        img = cv2.imread(img_name)
+        """
+        if conv2BW == True:
+            try:
+                img = rgb2gray(img)
+            except:
+                print("\An error occurred while saving the image in grayscale format. Applying the filters in RGB format.")
+        """
+        try:
+            # apply the filters
+            if filter == 'average':
+                blur = cv2.blur(img, (kernel_size, kernel_size))
+    
+            if filter == 'gaussian':
+                blur = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+    
+            if filter == 'median':
+                blur = cv2.medianBlur(img, kernel_size)
+            
+    
+            # if the filters can be applied, then display the image
+            try:
+                if display == True:
+                    plt.subplot(121),plt.imshow(img),plt.title('Original')
+                    plt.xticks([]), plt.yticks([])
+                    plt.subplot(122),plt.imshow(blur),plt.title(filter)
+                    plt.xticks([]), plt.yticks([])
+                    plt.show()
+                
+            # in case the image cannot be displayed
+            except:
+                print("\nAn error occurred while displaying the image.")
+        # in case the filtering process cannot be carried out
+        except:
+            print("\nThe kernel size is larger than the image dimensions.")
+            return img
+    # the file does not exist
+    except:
+        print("\nThe file does not exist in the directory.")
+        return 0
+        
+    return blur
